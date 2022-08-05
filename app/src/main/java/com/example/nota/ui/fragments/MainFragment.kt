9617371ed2f494d7aeb8b6a7a562bdee310671fa
ritik.malhotra7 <ritikrkmalhotra@gmail.com>
2022-08-06 04:02:05 +0530

    Initial Commit

--- app/src/main/java/com/example/nota/ui/fragments/MainFragment.kt
+++ app/src/main/java/com/example/nota/ui/fragments/MainFragment.kt
@@ -3,7 +3,6 @@ package com.example.nota.ui.fragments
 import android.app.Activity
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -43,7 +42,7 @@ class MainFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentMainBinding.inflate(layoutInflater)
         ctx = requireContext()
         activity = requireActivity()
